ec740593f4b7a8741974061cfb7c2df432c93d2e
Alieu Friggeri <afrigger@c2r2s1.42lausanne.ch>
2022-12-06 16:33:05 +0100

    error to fix in setlist (assignation du content depuis un int)

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: afrigger <afrigger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:10:39 by afrigger          #+#    #+#             */
-/*   Updated: 2022/12/06 11:39:10 by afrigger         ###   ########.fr       */
+/*   Updated: 2022/12/06 12:40:04 by afrigger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,8 @@ void	rotate_ab(t_list **pile_a, t_list **pile_b);
 void	revrotate_ab(t_list **pile_a, t_list **pile_b);
 void	printlist(t_list **pile_a, t_list **pile_b);
 void	delete(t_list **root);
+void	parse_arg(t_list **pile_a, char **argv, int argc);
+int		checkdouble(t_list **pile_a, int nb);
+void	setlist(t_list **pile_a, char **numbers);
+
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: afrigger <afrigger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:10:26 by afrigger          #+#    #+#             */
-/*   Updated: 2022/12/06 11:52:29 by afrigger         ###   ########.fr       */
+/*   Updated: 2022/12/06 16:32:05 by afrigger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,26 +73,96 @@ void	printlist(t_list **pile_a, t_list **pile_b)
 	ft_printf("[END OF LIST]\n");
 }
 
-void	parse_arg(t_list **pile_a, char **argv, int argc)
+void	setlist(t_list **pile_a, char **numbers)
 {
+	t_list	*tmp;
+	int		nb;
+	int		i;
 
+	tmp = *pile_a;
+	i = 1;
+	while(numbers[i])
+	{
+		nb = ft_atoi(numbers[i]);
+		if (checkdouble(pile_a, nb) == 1 ||
+			ft_strncmp(ft_itoa(nb), numbers[i], ft_strlen(ft_itoa(nb))) != 0)
+			{
+				ft_putstr_fd("Error\n", 2);
+				//ft_lstclear(pile_a, &delete);
+				return;
+			}
+			ft_printf("content is %d\n", tmp->content);
+		tmp->content = &nb; // probleme d-assignation a regler
+		tmp->next = ft_lstnew(0);
+		tmp = tmp->next;
+		i++;
+	}
+	tmp->next = NULL;
 }
 
-int main()
+int	checkdouble(t_list **pile_a, int nb)
+{
+	t_list	*tmp;
+
+	tmp = *pile_a;
+	if (tmp->content)
+	{
+		while (tmp != NULL)
+		{ //c ici le illegal ou le segfault
+			if (&nb == tmp->content)
+				return (1);
+			if (tmp->next == NULL)
+				return(0);
+			tmp = tmp->next;
+		}
+	}
+	return (0);
+}
+
+void	parse_arg(t_list **pile_a, char **argv, int argc)
+{
+	char **numbers;
+
+	if (argc <= 1)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return;
+	}
+	else if (argc == 2)
+	{
+		numbers = ft_split(argv[1], ' ');
+		setlist(pile_a, numbers);
+	}
+	else
+	{
+		numbers = argv;
+		setlist(pile_a, numbers);
+	}
+}
+
+int main(int argc, char **argv)
 {
 	t_list *pile_a;
 	t_list *pile_b;
+	int nb = 12;
+	//int * num = &nb;
 
-	pile_a = malloc(sizeof(t_list));
-	pile_b = malloc(sizeof(t_list));
-	pile_a->content = (int *)1;
-	pile_b->content = (int *)10;
-	ft_lstadd_back(&pile_a, ft_lstnew((int *)2));
-	ft_lstadd_back(&pile_a, ft_lstnew((int *)3));
-	ft_lstadd_back(&pile_a, ft_lstnew((int *)4));
-	ft_lstadd_back(&pile_b, ft_lstnew((int *)11));
-	ft_lstadd_back(&pile_b, ft_lstnew((int *)12));
-	ft_lstadd_back(&pile_b, ft_lstnew((int *)13));
+	pile_a = ft_lstnew((int *)1);
+	pile_b = ft_lstnew((int *)2);
+	(void)argc;
+	(void)argv;
+	parse_arg(&pile_a, argv, argc);
+	ft_printf("content is %d\n", pile_a->content);
+	pile_a->content = &nb;
+	ft_printf("content is %d\n", *(int *)pile_a->content);
+	// pile_a->content = (int *)1;
+	// pile_b->content = (int *)10;
+	// ft_lstadd_back(&pile_a, ft_lstnew((int *)2));
+	// ft_lstadd_back(&pile_a, ft_lstnew((int *)3));
+	// ft_lstadd_back(&pile_a, ft_lstnew((int *)4));
+	// ft_lstadd_back(&pile_b, ft_lstnew((int *)11));
+	// ft_lstadd_back(&pile_b, ft_lstnew((int *)12));
+	// ft_lstadd_back(&pile_b, ft_lstnew((int *)13));
 	printlist(&pile_a, &pile_b);
 	return 0;
 }
